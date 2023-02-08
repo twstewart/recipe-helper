d882ad2bb86009616ed15fae6478186716f560b5
tdubsxc <trdubs90@gmail.com>
2023-02-07 19:26:14 -0500

    Added functionality to fetch and display single recipe.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,10 @@ const form = document.querySelector("#form");
 const searchTextEl = document.querySelector("#search-text");
 const searchResultsEl = document.querySelector("#search-results");
 const mealsEl = document.querySelector("#meals");
+const mealEl = document.querySelector("#meal");
 
 form.addEventListener("submit", handleSearchRecipe);
+mealsEl.addEventListener("click", getMealById);
 
 async function handleSearchRecipe(e) {
   e.preventDefault();
@@ -24,6 +26,7 @@ async function handleSearchRecipe(e) {
   }
 
   form.reset();
+  mealEl.innerHTML = null;
 }
 
 function updateSearchResultsUI(searchTerm) {
@@ -48,7 +51,57 @@ function updateMealsUI(meals) {
   mealsEl.insertAdjacentHTML("afterbegin", html);
 }
 
+async function getMealById(e) {
+  if (e.target.matches(".meal-info")) {
+    const { mealId: id } = e.target.dataset;
+    const { meals } = await fetchSingleRecipe(id);
+    addMealToDOM(meals[0]);
+  }
+}
+
+function addMealToDOM(meal) {
+  const recipeInstructions = {};
+  let i = 1;
+
+  while (i <= 20 && meal[`strIngredient${i}`]) {
+    recipeInstructions[meal[`strIngredient${i}`]] = meal[`strMeasure${i}`];
+    i += 1;
+  }
+
+  mealEl.innerHTML = `
+    <div class="meal-wrapper">
+      <h2>${meal.strMeal}</h2>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="meal-details">
+        ${meal.strCategory ? `<h3>Category - ${meal.strCategory}</h3>` : null}
+        ${meal.strArea ? `<h4>Cuisine - ${meal.strArea}</h4>` : null}
+      </div>
+      <div class="content">
+        <h5>Instructions:</h5>
+        <p>${meal.strInstructions}</p>
+        <h6>Ingredients:</h6>
+        <ul>
+          ${Object.entries(recipeInstructions)
+            .map(([ingredient, measurement]) => {
+              return `
+              <li>
+                 ${ingredient} - ${measurement}
+              </li>
+            `;
+            })
+            .join("")}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
 function fetchRecipes(searchTerm) {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+  return fetcher(url);
+}
+
+function fetchSingleRecipe(recipeId) {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
   return fetcher(url);
 }
